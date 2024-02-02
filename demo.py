22a5536a9510e69f0754dce6e9f7c0a8f08907ee
Braiuz <45772711+Braiuz@users.noreply.github.com>
2024-02-02 12:53:07 +0100

    Added blink functionality handled by timer

--- demo.py
+++ demo.py
@@ -1,60 +1,29 @@
 import time
 from led import Led
-#import dht
+import ujson
 import bme280
 from machine import Pin, I2C, reset
 
 from constants import *
-from connectivity import *
-
-LED_OFF       = 0
-LED_ON        = 1
-LED_SLOWBLINK = 2
-LED_FASTBLINK = 3
-
-measured = False	# True if measure succeded
-sensor: bme280.BME280
-led: Led
-
-def LedSignaling(ledStatus: int):
-    # 0 -> off
-    # 1 -> solid on
-    # 2 -> slow blink
-    # 3 -> fast blink
-    global led
-    if (LED_OFF == ledStatus):
-        pass
-    elif (LED_ON == ledStatus):
-        led.on()
-    elif ( LED_SLOWBLINK == ledStatus):
-        while(True):
-            led.on()
-            time.sleep(1)
-            led.off()
-            time.sleep(1)
-    elif (LED_FASTBLINK == ledStatus):
-        while(True):
-            led.on()
-            time.sleep(0.2)
-            led.off()
-            time.sleep(0.2)
-            
-            
+from connectivity import *            
+         
+         
 def Demo_Init():
     global sensor
     global led
     led = Led()
+    led.blink(led.LED_SLOW_PERIOD_MS)
     #client = MakeConnections()
-    print("BME280 init")
     i2c=I2C(0, sda=Pin(I2C_SDA_GPIO), scl=Pin(I2C_SCL_GPIO), freq=I2C_FREQ_HZ)
-    #try:
-    sensor = bme280.BME280(i2c=i2c)
-    #except Exception as e:
-    #    print('Failed BME280 init - ' + str(e))
-        #machine.reset()
-    print("Done")
-
-    #sensor = dht.DHT11(Pin(28))
+    try:
+        sensor = bme280.BME280(i2c=i2c)
+    except Exception as e:
+        print('Failed BME280 init - ' + str(e))
+        led.blink(led.LED_FAST_PERIOD_MS)
+        while(True):
+            time.sleep(1)
+    print("BME280 init done")
+    led.off()
 
     time.sleep(2)
 
@@ -62,24 +31,26 @@ def Demo_Init():
 def Demo_Task():
     global measured
     global led
+    
+    global temp
+    global press
+    global hum
+    
     while (not measured):
         try:
-            #sensor.measure()
-            print(sensor.values)
+            temp, press, hum = sensor.values
+            print("Temp=" + temp + "°C - Press=" + press + "hPa - Hum=" + hum + "%")
             measured = True
         except OSError as e:
              print('Failed to read sensor.')
-             time.sleep(1)
+             led.blink(LED_FAST_PERIOD_MS)
+             while(True):
+                 time.sleep(1)
     measured = False
-    #temp = sensor.temperature()
-    #hum = sensor.humidity()
     timestamp = time.time()
     # Json encode
-    #sensorData = {"temperature":temp, "humidity":hum, "timestamp":timestamp}
+    #sensorData = {"timestamp":timestamp, "temperature":temp, "pressure":press, "humidity":hum}
     #jsonData = ujson.dumps(sensorData)
-    
     #Publish("TestSensor", jsonData, client)
-    #Publish("TestHumidity", str(hum), client)
     time.sleep(2)
-    led.toggle()
     
--- led.py
+++ led.py
@@ -1,8 +1,13 @@
-from machine import Pin
+from machine import Pin, Timer
 
 class Led:
     _state: bool
     _pin: Pin
+    _timer: Timer	# ms
+    _initT = False
+    
+    LED_SLOW_PERIOD_MS = 1000
+    LED_FAST_PERIOD_MS = 200
 
     def __init__(self):
         self._pin = Pin("LED", Pin.OUT)
@@ -11,9 +16,22 @@ class Led:
     
     def toggle(self):
         self._pin.toggle()
+        
+    def _Toggle_Cb(self, timer):
+        self._pin.toggle()
     
     def off(self):
         self._pin.off()
 
     def on(self):
         self._pin.on()
+        
+    def blink(self, period_ms):
+        if (False == self._initT):
+            self._timer = Timer()
+            self._timer.init(mode=Timer.PERIODIC, period=period_ms, callback=self._Toggle_Cb)
+            self._initT = True
+        else:
+            self._timer.deinit()
+            self._timer.init(mode=Timer.PERIODIC, period=period_ms, callback=self._Toggle_Cb)
+            self._initT = True
