05b5491dd4772acf319e94fed3d7174987fd6c3a
Braiuz <45772711+Braiuz@users.noreply.github.com>
2024-02-09 09:59:05 +0100

    Delete constants.py
    Privacy reasons

--- constants.py
+++
@@ -1,7 +0,0 @@
-SERVER_HOSTNAME = "192.168.178.44"#"fcc869f509a44620b30206677c02ad0e.s2.eu.hivemq.cloud"
-SERVER_PORT = 8883
-USER = 'matteo'#"Matteo"
-PASSWORD = 'matteo'#"ecbhchdab$MQTT6"
-
-WLAN_NAME = "Barbagianni"
-WLAN_PASSWORD = "ciccinivolantifulminati"
